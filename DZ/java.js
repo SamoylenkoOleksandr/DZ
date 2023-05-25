e02b60628879b5a9cc1d7e7ccc498f5851abbe74
SamoylenkoOleksandr <wargamingantinoob@gmail.com>
2023-05-25 15:31:25 +0300

    hj,jnf

---
+++ DZ/java.js
@@ -0,0 +1,28 @@
+//Розділ Масиви
+// 1
+{
+    const confirms = [confirm()]
+    confirms
+}
+// 2
+{ 
+    const arr = [1,2,3,4] 
+    const arr1 = arr
+    arr === arr
+}
+// 3 
+{
+    const access = [prompt().length]
+    access
+}
+// 4 (Половина)
+{
+    const question = [prompt()]
+    const question1 =question
+    question1
+}
+// 5
+{
+    const calculator = [[0,0,0,0,0,0],[0,1,2,3,4,5],[0,2,4,6,8,10],[0,3,6,9,12,15],[0,4,8,12,16,20],[0,5,10,15,20,25]]
+    let results =alert(calculator[2][3])
+}
