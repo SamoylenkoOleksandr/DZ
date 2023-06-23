184508bd566c6cd1f02fbb75ecf62d1b376fb419
SamoylenkoOleksandr <wargamingantinoob@gmail.com>
2023-06-23 19:10:13 +0300

    shoto

--- DZ/Data type/javascript.js
+++ DZ/Data type/javascript.js
@@ -1,3 +1,4 @@
+//  One lisening
 //  {
 //     var a = 5; 
 //     var b, c;
@@ -5,21 +6,21 @@
 //     b = c = b/2;
 //     prompt(b)
 // }
-
+//    Two lisining
 // {
 //     let question = +prompt('Вкажіть скільки вам років ?')
 //     let year = 2023
 //     let result = year - question
 //     alert(`Ви ${result} року народження`)
 // }
-
+//  Thee lisining 
 // {
 //     let question = +prompt('Вкажіть скільки *С потрібно перетворити в Фаренгейт ?')
 //     let fahrenheit = 273    
 //     let result = fahrenheit + question
 //     alert(`Задана вами температу в Фаренгейтах буде  ${result}`)
 // }
-
+//  Foor lisining 
 // {
 //     let question = +prompt('Вкажіть число')
 //     let question1 = +prompt('Вкажіть число')
@@ -27,7 +28,7 @@
 //     let result =Math.floor(division)
 //     alert(`Ваш результат ${result}`)
 // }
-
+//  Five lisining
 // {
 //     const rate = +prompt('Введіть курс валюти')
 //     const question = +prompt('Введіть суму яку хочете перевести в гривні ₴')
@@ -35,12 +36,12 @@
 //     const resultat = result.toFixed(2)
 //     alert(`Сума в гривнях дорівнює: ${resultat}`)
 // }
-
-{
-    let floor = prompt("Введіть кількість поверхів");
-    let apartment = prompt("Введіть кількість квартир на поверсі");
-    let room = prompt("Введіть номер квартири");
-    let entrance = Math.ceil(room / (floor * apartment));
-    let floor1 = Math.ceil((room % (floor * apartment)) / apartment);
-    alert(`Під'їзд номер ${entrance}\nПоверх номер ${floor1}`);
-}
+//  Seven lisining
+// {
+//     let floor = prompt("Введіть кількість поверхів");
+//     let apartment = prompt("Введіть кількість квартир на поверсі");
+//     let room = prompt("Введіть номер квартири");
+//     let entrance = Math.ceil(room / (floor * apartment));
+//     let floor1 = Math.ceil((room % (floor * apartment)) / apartment);
+//     alert(`Під'їзд номер ${entrance}\nПоверх номер ${floor1}`);
+// }
---
+++ DZ/String/javascript.js
@@ -0,0 +1,29 @@
+// // One lisining
+// {
+// let question = prompt('Вкажіть своє ім`я')
+// alert (`Привіт ${question}`)
+// }
+
+// two lisining
+// {
+//     let question = prompt('Задайте питання')
+//     let split = question.split(' ')
+//     let join = split.join(' , блін ')
+//     alert(join)
+// }
+// tree lisining
+{
+    let str = "cANBerRa";
+    let result = str.slise(1).toUpperCase() + str.slice(1) + 'C'.toLowerCase();
+    console.log(result); // Canberra
+    
+
+
+
+
+}
+
+
+
+
+
