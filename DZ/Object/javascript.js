14160a9874b4bcb9e2cc704590590c489f1fcbb9
SamoylenkoOleksandr <wargamingantinoob@gmail.com>
2023-07-05 14:24:10 +0300

    Object homeworc

---
+++ DZ/Object/javascript.js
@@ -0,0 +1,71 @@
+// One lisining
+// const phone = {
+//     brand: 'Xiaomi',
+//     Model: 'M2010J19CG',
+//     VersualAndroid: '12 SKQ1.211202.001',
+// }
+
+// Two lisining
+
+// const nameKey = prompt('Введіть потрібний ключ')
+// const phone = {
+//     brand:`${[nameKey]}`, 
+// }
+
+// Tree lisining
+
+// const [nameKeyBrand,nameKeyModel,nameKeyVersualAndroid] = [prompt('Enter the Brand name of your phone'),prompt('Enter the Model name of your phone'),prompt('Enter your phone \'s Android version')]
+// const phone = {
+//     Characteristic: [{
+//         Brand: `${nameKeyBrand}`,
+//         Model: `${nameKeyModel}`,
+//         VersualAndroid: `${nameKeyVersualAndroid}`,
+//     }
+//     ]
+// }
+// console.log(phone)
+
+// Four lisining
+
+const JSONstructuration = {
+    tagName: [
+        {
+            children: [
+                {
+                    '<div>': [
+                        {
+                            '<span>': 'Enter a data please :',
+                            attrs: [
+                                {
+                                    '<br>': [
+                                        {
+                                            '<input type>': 'text',
+                                            '<id>': 'name',
+                                            '<input type>': 'text',
+                                            '<id>': 'surname',
+        
+                                        
+                                        }
+                                    ]
+                                }
+                            ]
+
+                            
+                        }
+                        
+                    ],
+                    '<div>': [
+                        {
+                            '<button id>': 'ok',
+                            '<button id>': 'cansel',
+                            
+                        }
+                    ]
+                }
+
+            ]
+
+            
+        }
+    ]
+}
